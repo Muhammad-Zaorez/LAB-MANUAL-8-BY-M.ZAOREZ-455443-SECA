3f7686aec72707b0c7e1619a43997460f4cdfa49
Muhammad-Zaorez <147588674+Muhammad-Zaorez@users.noreply.github.com>
2023-11-28 23:09:38 -0800

    Add files via upload

---
+++ lab manual 08 by M.Zaorez 455443.cpp
@@ -0,0 +1,75 @@
+/*
+                                  LAB MANUAL 8
+                                M.ZAOREZ (455443)
+                                     SEC-A
+*/
+#include<iostream>
+/*using namespace std;
+int main()
+{
+ //                                     TASK 01
+	int arr[5];
+	for(int i=0;i<=4;i++){
+		cout<<"enter integer: "<<endl;
+		cin>>arr[i];
+	}
+	cout<<"The average of 5 numbers is:\n "<<(arr[0]+arr[1]+arr[2]+arr[3]+arr[4])/5;
+ 
+ //                                      TASK 02
+ /*int n[5];
+ int temp;
+ for(int i=0;i<=4;i++){
+ 	cout<<"enter numbers: "<<endl;
+ 	cin>>n[i];
+ }
+ for(int i=0;i<=4;i++){
+ 	for(int j=0;j<=3;j++){
+ 		if(n[j]>n[j+1]){
+ 			temp=n[j];
+ 			n[j]=n[j+1];
+ 			n[j+1]=temp;
+		}
+    }
+    
+ }
+ for(int i=0;i<=4;i++){
+ 	cout<<n[i];
+ }
+ */
+//                                  TASK 03
+/*int arr[5],i,j,min,temp;
+	for(i=0;i<5;i++){
+	
+	cout<<"enter array of 5 elements: "<<endl;
+	cin>>arr[i];
+	}
+	cout<<"the original  values in array:"<<endl;
+	for(i=0;i<5;i++){
+		cout<<arr[i]<<" ";
+		
+	}
+	for(i=0;i<4;i++){
+		min=i;
+		for(j=i+i;j<5;j++){
+			if(arr[j]<arr[min])
+			min=j;
+			}
+		if   (min!=i){
+		
+	
+	temp=arr[i];
+	arr[i]=arr[min];
+	arr[min]=temp;
+	
+	}
+	
+	}
+	cout<<"the sorted array: "<<endl;
+	for(i=0;i<5;i++)
+{
+	cout<<arr[i]<<" ";
+
+}
+*/
+
+}
